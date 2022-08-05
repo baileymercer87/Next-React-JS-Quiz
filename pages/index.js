1d9d2a7ce4c8932d1ec98db53a9b51dc73a42bec
Baileymercer87 <bailey_mercer@hotmail.co.uk>
2022-08-05 16:03:48 +0100

    Add redirects on click
    Simple redirect - > page not working yet

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,30 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Router from "next/router";
 
 export default function Home() {
   const quiz_array = [["eurocap", "eurocap.json", "eurocap.png"]];
-    return (<div>
-        <Head>
-            <title>Quiz homepage</title>
-        </Head>
-        <header className={styles.header}>
-            <h1 className={styles.h1}>General Knowledge Quiz</h1>
-            <h2>Select your quiz option from below:</h2>
-        </header>
-        <section className={styles.section}>
-            <ul className={styles.list}>
-            {quiz_array.map((quiz) => {
-                return <li className={styles.listItem} key={quiz[2]}>{quiz[0]}</li>
-            })}
-            </ul>
-        </section>
-    </div>);
+
+  function handleSelection (selection) {
+    const { pathname } = Router;
+    if (pathname === "/") {
+      Router.push("/" + selection);
+    }
+  }
+  return (<div>
+    <Head>
+      <title>Quiz homepage</title>
+    </Head>
+      <header className={styles.header}>
+        <h1 className={styles.h1}>General Knowledge Quiz</h1>
+        <h2>Select your quiz option from below:</h2>
+      </header>
+      <section className={styles.section}>
+        <ul className={styles.list}>
+          {quiz_array.map((quiz) => {
+             return <li className={styles.listItem} key={quiz[2]} onClick={event => handleSelection(quiz[0])}>{quiz[0]}</li>
+          })}
+        </ul>
+     </section>
+   </div>);
 }
